393c52c7742f0501ca96fb554c6f78c2e3014a3c
redowanhossain <redowan.bu.cse@gmail.com>
2023-05-25 01:06:25 +0600

    template mastering done

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('backend.layouts.master');
+        return view('backend.pages.dashboard.index');
     }
 }
